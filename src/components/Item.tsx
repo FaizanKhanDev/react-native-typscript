f384ee2ae7aa266a0e228810fc3067aa1c3d5a77
faizankhandev <faizan480khan@gmail.com>
2024-04-28 01:12:25 -0700

    FIXED: Created TODO List using reactnative ts

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,18 +1,42 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { IItem } from './AddItem';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Import the icon library
 
-interface Props {
-    item: string
-    quantity: number
-}
+const Item: React.FC<IItem> = ({ item, quantity }) => {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.itemName}> Item: {item}</Text>
+      <Text style={styles.quantity}> Quantity: {quantity}</Text>
+      <TouchableOpacity style={styles.iconContainer}>
+        <Icon name="pencil" size={20} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => console.log('deleted')} style={styles.iconContainer}>
+        <Icon name="trash" size={20} color="red" />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-const Item: React.FC<Props> = ({item, quantity }) => {
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  itemName: {
+    fontSize: 16,
+  },
+  quantity: {
+    fontSize: 14,
+    color: '#666',
+  },
+  iconContainer: {
+    padding: 5,
+  },
+});
 
-    return (
-        <View>
-            <Text>{item} - {quantity}</Text>
-        </View>
-    )
-}
-
-export default Item
+export default Item;
