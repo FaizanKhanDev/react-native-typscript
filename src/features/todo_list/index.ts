d03cd8b8a8984affa33c2a6b65ce9536a653a675
faizankhandev <faizan480khan@gmail.com>
2024-04-28 05:28:57 -0700

    FIXINEDL To Conenct redux

---
+++ src/features/todo_list/index.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Todo {
+    id: number;
+    name: string;
+    quantity: number;
+}
+interface TodoListState {
+    todoList: Todo[]
+}
+
+const initialState: TodoListState = {
+    todoList: [
+        { id: 1, name: "John", quantity: 20 },
+        { id: 2, name: "Jane", quantity: 21 },
+        { id: 3, name: "Jack", quantity: 22 },
+        { id: 4, name: "Jill", quantity: 23 },
+        { id: 5, name: "Joe", quantity: 24 }
+    ]
+};
+
+const todoListSlice = createSlice({
+    name:"TodoList",
+    initialState,
+    reducers: {
+            createItem :(state, action:PayloadAction<Todo>) => {
+                state.todoList.push(action.payload)
+                console.log("List", state.todoList);
+            }
+    }
+
+})
+
+export  const {
+    createItem
+} = todoListSlice.actions;
+
+export default todoListSlice.reducer
